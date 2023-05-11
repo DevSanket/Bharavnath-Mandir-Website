4222e6aa7bfa211e531dc8a036621cadd4d2d842
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2023-05-11 12:38:54 +0530

    Working on msg

--- pages/addDengi.jsx
+++ pages/addDengi.jsx
@@ -217,7 +217,7 @@ const AddDengi = () => {
           </div>
           <div className="text-center my-5">
             <a
-              href={`sms:+91${mobile};?&body=देणगी मिळाली मंदिर संवर्धन समिती आपली आभारी आहे. कृपया आपण आपली पावती या वेबसाईटवर जाऊन पहा https://bhairavnath-dengidar.vercel.app/login`}
+              href={`sms:+91${mobile};?&body=आपली देणगी मिळाली, भैरवनाथ मंदिर संवर्धन समिती आपली आभारी आहे. कृपया आपण आपली पावती या वेबसाईटवर जाऊन पहा https://bhairavnath-dengidar.vercel.app/login`}
             >
               Send SMS
             </a>
